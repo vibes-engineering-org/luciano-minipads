03ba97163203e4f15722fbb5b319e05ceff90008
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-09-22 18:19:54 +0000

    Initial commit from template

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const PROJECT_TITLE = "Minipads";
+export const PROJECT_DESCRIPTION = "by @luciano";
+export const PROJECT_CREATOR = "luciano";
+export const PROJECT_AVATAR_URL =
+  "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
